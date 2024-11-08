6feacb6f94adafe89d9df0985cf8d7aea59d2dad
lenovo <omkar.reddy211@gmailcom>
2024-11-08 19:56:48 +0530

    payment script

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "tools" {
     source = "./module-ec2"
     for_each = var.tools
     tool_name = each.key
-    instance_type = each.value[instance_type]
-    sg_port = each.value[port]
-    volume_size = each.value[volume_size]
+    instance_type = each.value["instance_type"]
+    sg_port = each.value["port"]
+    volume_size = each.value["volume_size"]
     domain_name = var.domain_name
     zone_id = var.zone_id
     }
