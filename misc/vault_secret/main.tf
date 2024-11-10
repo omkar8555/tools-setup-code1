4590699fe193d9755ee20ed509239cd04d769c48
lenovo <omkar.reddy211@gmailcom>
2024-11-10 22:41:45 +0530

    payment script

--- misc/vault_secret/main.tf
+++ misc/vault_secret/main.tf
@@ -23,7 +23,7 @@ resource "vault_mount" "roboshop-dev" {
 }
 
 resource "vault_generic_secret" "frontend" {
-  path = "roboshop-dev/frontend"
+  path = "${vault_mount.roboshop-dev.path}/frontend"
 
   data_json = <<EOT
   { "foo" = "bar"
