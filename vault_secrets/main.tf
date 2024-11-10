b06b9be57e0a22b6fbcd9addfdaeca99af7e372d
lenovo <omkar.reddy211@gmailcom>
2024-11-10 22:12:29 +0530

    payment script

---
+++ vault_secrets/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d80"
+    key    = "vault-secrets/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
+
+provider "vault" {
+  address         = "http://vault-internal.rdevopsb72.shop:8200"
+  token           = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
