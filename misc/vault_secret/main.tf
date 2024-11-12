1c8429d1557cb308f22372db904bb6316a3700f8
lenovo <omkar.reddy211@gmailcom>
2024-11-12 11:51:36 +0530

    payment script

--- misc/vault_secret/main.tf
+++ misc/vault_secret/main.tf
@@ -26,8 +26,11 @@ resource "vault_generic_secret" "frontend" {
   path = "${vault_mount.roboshop-dev.path}/frontend"
 
   data_json = <<EOT
-  { "foo": "bar",
-  "pizza": "cheese"
-  }
+  { "catalogue_url" = "http://catalogue-dev.rdevopsb72.shop:8080/",
+   "cart_url" = "http://cart-dev.rdevopsb72.shop:8080/",
+   "user_url" = "http://user-dev.rdevopsb72.shop:8080/",
+   "payment_url" = "http://payment-dev.rdevopsb72.shop:8080/",
+   "shipping_url" = "http://shipping-dev.rdevopsb72.shop:8080/"
+     }
   EOT
   }
